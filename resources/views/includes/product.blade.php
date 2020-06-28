901afa732e64aae33e49398cefab580094bceefa
Leonie Dusart <leonie.dusart@gmail.com>
2020-06-28 16:44:16 +0200

    Little fix on available colors display

--- resources/views/includes/product.blade.php
+++ resources/views/includes/product.blade.php
@@ -42,9 +42,11 @@
         </button>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby={{"dropdownMenu".$product->id}}>
             @foreach($product->colors as $key => $color)
-                <a class="dropdown-item" href=" {{ route('panier.add', ['product' => $product, 'color' => $color]) }}">
-                    {{ __($color->name) }}
-                </a>
+                @if($color->pivot->stock > 0)
+                    <a class="dropdown-item" href=" {{ route('panier.add', ['product' => $product, 'color' => $color]) }}">
+                        {{ __($color->name) }}
+                    </a>
+                @endif
             @endforeach
         </div>
     </div>
